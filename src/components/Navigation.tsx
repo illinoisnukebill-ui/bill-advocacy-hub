8c673f6a756db883a366959c5d6954b32eb7f3e9
illinoisnukebill-ui <illinoisnukebill@gmail.com>
2025-11-17 19:56:37 -0600

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -11,7 +11,7 @@ const Navigation = () => {
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <NavLink to="/" className="text-xl font-bold hover:opacity-80 transition-opacity">
-            Citizens' Rights Act
+            Nuclear Operationalization Act
           </NavLink>
 
           {/* Desktop Navigation */}
