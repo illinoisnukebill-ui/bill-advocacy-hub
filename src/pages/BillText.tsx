6b373359f42310f3f87f6d78e02cf7562b9b02b1
illinoisnukebill-ui <illinoisnukebill@gmail.com>
2025-11-17 20:05:45 -0600

    Update BillText.tsx

--- src/pages/BillText.tsx
+++ src/pages/BillText.tsx
@@ -39,7 +39,7 @@ export default function BillText() {
             )}
 
             <iframe
-              src="ThermonuclearEnergyGenerationDevice_Draft1.pdf"
+              src="/public/ThermonuclearEnergyGenerationDevice_Draft1.pdf"
               className="w-full h-full"
               title="PDF Viewer"
               onLoad={() => setLoading(false)}
